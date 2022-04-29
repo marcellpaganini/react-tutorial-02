e1de4665afaa50fd871dc45b87f35a173fd4943c
Marcell Tanure <paganini.marcell@gmail.com>
2022-04-29 12:29:19 -0300

    useEffect applied to observe changes and add to localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
 import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
 function App() {
   const [todos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos])
 
   const handleAddTodo = (e) => {
     const name = todoNameRef.current.value;
