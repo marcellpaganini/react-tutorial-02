4d8a6701975632c8e2d7aaa4502f6b8ad09b4d61
Marcell Tanure <paganini.marcell@gmail.com>
2022-04-29 13:21:50 -0300

    Keeping state on refresh

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
   const todoNameRef = useRef();
 
   useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
   }, [todos])
 
