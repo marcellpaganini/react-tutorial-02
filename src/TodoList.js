c8c7066354aac4a8d6ce27abac8dde965b33f8ee
Marcell Tanure <paganini.marcell@gmail.com>
2022-04-29 10:00:56 -0300

    State implemented

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
-    <div>TodoList</div>
+    <div>{todos.length}</div>
   )
 }
